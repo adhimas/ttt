68d4d9c18d38fb49ec819bc6b082bc2c2a49b2b3
adhimas <adhimas@users.noreply.github.com>
2026-08-07 19:38:24 +0200

    Handle game moves

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 
 	"example.com/ttt/internal/messaging"
+	"example.com/ttt/internal/tictactoe"
 )
+
+// TODO: scope variables properly, e.g., pass to handler as a dependency
 
 var upgrader = websocket.Upgrader{} // TODO: configure
 
 var clients = make(chan struct{}, 100)
 
-var lobby chan struct{}
+var lobby chan tictactoe.Subscription
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	select {
@@ -37,25 +40,67 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	// ctx := r.Context()
 	// TODO
-	_ = <-lobby // select, timeout/interrupt
+	game := <-lobby // select, timeout/interrupt
 
 	for {
-		err = c.WriteJSON(messaging.Message{Code: messaging.Other, Text: "test"})
+		update, ok := <-game
+		if !ok {
+			log.Println("no update")
+			return
+		}
+
+		msg := messaging.Message{
+			Code: messaging.StatusUpdate,
+			Payload: messaging.Payload{
+				StatusUpdate: &update,
+			},
+		}
+		err = c.WriteJSON(msg)
 		if err != nil {
 			log.Println("write:", err)
 			break
 		}
-		time.Sleep(time.Second)
+
+		if update.Winner != nil {
+			break
+		}
+
+		msg = messaging.Message{
+			Code: messaging.Prompt,
+			Payload: messaging.Payload{
+				Prompt: update.Piece,
+			},
+		}
+		err = c.WriteJSON(msg)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+
+		var respond messaging.Message
+		err := c.ReadJSON(&respond)
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		if respond.Code != messaging.Move {
+			log.Println("msg code:", err)
+			break
+		}
+
+		update.NextMove <- respond.Payload.Move
 	}
 }
 
 func main() {
-	lobby = make(chan struct{})
+	lobby = make(chan tictactoe.Subscription)
 	go func() {
 		for {
-			lobby <- struct{}{}
-			lobby <- struct{}{}
-			// TODO
+			p1 := make(chan tictactoe.GameUpdate, 1)
+			p2 := make(chan tictactoe.GameUpdate, 1)
+			lobby <- p1
+			lobby <- p2
+			tictactoe.Start(context.Background(), p1, p2)
 		}
 		// TODO: exit
 	}()
