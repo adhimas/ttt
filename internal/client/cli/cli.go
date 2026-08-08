07e365ee56ff22a51616a2604ec4e6b9abef3576
adhimas <adhimas@users.noreply.github.com>
2026-08-08 08:15:51 +0200

    Put exported functions at top

--- internal/client/cli/cli.go
+++ internal/client/cli/cli.go
@@ -11,35 +11,6 @@ import (
 	"example.com/ttt/internal/tictactoe"
 )
 
-func PrintGame(game *tictactoe.GameUpdate) {
-	for i, cell := range game.Board {
-		if cell == tictactoe.Empty {
-			fmt.Printf(" %d", i)
-		} else {
-			fmt.Printf(" %s", cell.String())
-		}
-
-		if i%3 == 2 {
-			fmt.Println()
-		}
-	}
-
-	if game.Winner != nil {
-		printWinner(game.Piece, *game.Winner)
-	}
-}
-
-func printWinner(player, winner tictactoe.Cell) {
-	switch winner {
-	case tictactoe.Empty:
-		fmt.Println("tied!")
-	case player:
-		fmt.Println("you won!")
-	default:
-		fmt.Println("game over")
-	}
-}
-
 func Prompt(player tictactoe.Cell) int {
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
@@ -67,5 +38,34 @@ func Prompt(player tictactoe.Cell) int {
 
 		fmt.Println()
 		return cellNum
+	}
+}
+
+func PrintGame(game *tictactoe.GameUpdate) {
+	for i, cell := range game.Board {
+		if cell == tictactoe.Empty {
+			fmt.Printf(" %d", i)
+		} else {
+			fmt.Printf(" %s", cell)
+		}
+
+		if i%3 == 2 {
+			fmt.Println()
+		}
+	}
+
+	if game.Winner != nil {
+		printWinner(game.Piece, *game.Winner)
+	}
+}
+
+func printWinner(player, winner tictactoe.Cell) {
+	switch winner {
+	case tictactoe.Empty:
+		fmt.Println("tied!")
+	case player:
+		fmt.Println("you won!")
+	default:
+		fmt.Println("game over")
 	}
 }
